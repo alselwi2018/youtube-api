0f6829882e0940a17abce65febfc8c6461e8969d
alselwi2018 <abdul@Abdus-MacBook-Pro.local>
2020-01-31 23:21:58 +0000

    added the youtube videoid, title and description

---
+++ public/api.js
@@ -0,0 +1,36 @@
+
+/* @author Florian Fries (contact@florianfries.me) */
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+$(function(){
+    $("form").on("submit", function(e){
+        e.preventDefault();
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 10,
+            order: "viewCount",
+            publishedAfter: "2019-01-01T00:00:00Z"
+        });
+        request.execute(function(response){
+           var res = response.result;
+           $.each(res.items, function(i, v){
+
+            $.get("item.html",function(data){
+                $('#res').append(tplawesome(data, [{"title":v.snippet.title,"videoid":v.id.videoId,"description":v.snippet.description}]));
+                });
+
+           })
+        });
+    })
+});
+
+function init(){
+    gapi.client.setApiKey("AIzaSyA9l0tuo2zcxdjYs2gPfCmo5JructSFwjU");
+    gapi.client.load("youtube","v3",function(){
+
+    });
+}
+
+
+
