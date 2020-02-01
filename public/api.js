4501a422df5903161b31a4ef6155e8ea3f3e895e
alselwi2018 <abdul@Abdus-MacBook-Pro.local>
2020-02-01 08:59:59 +0000

    removed title

--- public/api.js
+++ public/api.js
@@ -4,21 +4,23 @@ function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{
 $(function(){
     $("form").on("submit", function(e){
         e.preventDefault();
+        var q = $("#query").val();
         var request = gapi.client.youtube.search.list({
             part: "snippet",
             type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            // q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            q: q,
             maxResults: 10,
             order: "viewCount",
-            publishedAfter: "2019-01-01T00:00:00Z"
+            publishedAfter: "2020-02-01T00:00:00Z"
         });
         //execute
         request.execute(function(response){
            var res = response.result;
            $.each(res.items, function(i, v){
-
+           var url = "https://www.youtube.com/watch?v="+v.id.videoId;
             $.get("allVideos.html",function(data){
-                $('#res').append(tplawesome(data, [{"title":v.snippet.title,"videoid":v.id.videoId,"description":v.snippet.description}]));
+                $('#res').append(tplawesome(data, [{"videoid":v.id.videoId,"description":v.snippet.description,"thumbnails":  url}]));
                 });
 
            })
@@ -27,7 +29,7 @@ $(function(){
 });
 
 function init(){
-    gapi.client.setApiKey("Add your api key here");
+    gapi.client.setApiKey("AIzaSyA9l0tuo2zcxdjYs2gPfCmo5JructSFwjU");
     gapi.client.load("youtube","v3",function(){
 
     });
