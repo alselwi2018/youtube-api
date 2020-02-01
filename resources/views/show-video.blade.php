f4f225fee362209c59bc310b6327b8d7bb94f4a8
alselwi2018 <abdul@Abdus-MacBook-Pro.local>
2020-02-01 00:07:25 +0000

    show-video search form

---
+++ resources/views/show-video.blade.php
@@ -0,0 +1,15 @@
+@extends('welcome')
+@section('content')
+ <div class="col-sm-12 text-center p-6"> Search for youtube videos
+    </div>
+  <div class="p-4">
+        {!! Form::open(['class'=>['action'=>'#','method'=>'post']]) !!}
+        {!! Form::text('youtube', '', ['class'=>'form-control','id'=>'search','placeholder'=>'search for a video']) !!}
+        <div class="text-center pt-4">
+        {!! Form::submit('Search', ['class'=>'btn btn-primary p-2']) !!}
+        </div>
+        {!! Form::close() !!}
+    </div>
+    //video displays in the below div
+    <div id="res" class="pt-3"></div>
+    @endsection
